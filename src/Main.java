86dd8ef75e0ebc33f708b5ead98a03c0bdadbbaa
SofiAlfonso <asalfonsom@eafit.edu.co>
2023-10-31 19:19:38 -0500

    final

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,7 @@ import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
-import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.regex.Pattern;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
